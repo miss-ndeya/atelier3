6e646455068bef4f3bc6eb8b429bfad06c115de7
unknown <miss-ndeya>
2023-06-12 18:11:41 +0200

    ajout

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function listeTache() {
     let valeur = document.getElementById("Myinput").value;
-    document.getElementById("text").innerHTML = "voici l'element que vous avait saisi: " + valeur
+    document.getElementById("text").innerHTML = "Vous avait saisi: " + valeur
     console.log(valeur); 
 }
